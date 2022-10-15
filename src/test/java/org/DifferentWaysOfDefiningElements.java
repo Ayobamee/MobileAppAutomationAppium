a5cbc9789cf5ea09c3cc472ce0df0991a7c7ec14
Ayobamee <ayobami.elutade@com>
2022-10-15 15:21:00 +0100

    added different ways of defining elements

---
+++ src/test/java/org/DifferentWaysOfDefiningElements.java
@@ -0,0 +1,41 @@
+package org;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import io.appium.java_client.pagefactory.iOSXCUITFindBy;
+
+import org.example.StartDriverSession;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DifferentWaysOfDefiningElements {
+    @AndroidFindBy(xpath = "//*[@text=\"Accessibility\"]")
+    @iOSXCUITFindBy(accessibility = "Accessibility")
+    private static MobileElement myElement3;
+
+    public DifferentWaysOfDefiningElements(AppiumDriver driver){
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = StartDriverSession.initializeDriver("Android");
+        DifferentWaysOfDefiningElements differentWaysOfDefiningElements = new DifferentWaysOfDefiningElements(driver);
+        System.out.println(myElement3.getText());
+
+        By myElement2 = MobileBy.AccessibilityId("Accessibility");
+        System.out.println(driver.findElement(myElement2).getText());
+
+        MobileElement myElement = (MobileElement) driver.findElementByAccessibilityId("Accessibility");
+        System.out.println(myElement.getText());
+
+        WebElement myElement1 = driver.findElementByAccessibilityId("Accessibility");
+        System.out.println(myElement1.getText());
+    }
+}
+
+// MobileElement, WebElement, By, MobileBy, @FindBy, @AndroidFindBy, @iOSXCUITFindBy
