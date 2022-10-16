d13b3018f6afe689e1a029e4a8eee3d811550a1f
Ayobamee <ayobami.elutade@com>
2022-10-16 08:50:11 +0100

    added waitc commands

---
+++ src/test/java/org/Waits.java
@@ -0,0 +1,30 @@
+package org;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+
+import org.example.StartDriverSession;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class Waits {
+
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = StartDriverSession.initializeDriver("iOS");
+   //     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        By alertViews = MobileBy.AccessibilityId("Alert Views");
+        By okayCancel = MobileBy.AccessibilityId("Okay / Cancel");
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(alertViews)).click();
+     //   driver.findElement(alertViews).click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(okayCancel)).click();
+     //   driver.findElement(okayCancel).click();
+
+// why not to use both implicit and explicit wait !!!???
+    
+    }}
