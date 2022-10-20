b5fa5b5b17184add1a4e13599919d486c70632f2
Ayobamee <ayobami.elutade@com>
2022-10-20 08:25:37 +0100

    added gesturetap method

---
+++ src/test/java/org/GesturesTap.java
@@ -0,0 +1,29 @@
+package org;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import io.appium.java_client.touch.offset.PointOption;
+
+import org.example.StartDriverSession;
+import org.openqa.selenium.By;
+
+import java.util.concurrent.TimeUnit;
+
+class GesturesTap {
+
+    public static void main(String[] args) throws Exception {
+        AppiumDriver driver = StartDriverSession.initializeDriver("Android");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        By accessibility = MobileBy.AccessibilityId("Accessibility");
+
+        TouchAction t = new TouchAction(driver);
+      //  t.tap(ElementOption.element(driver.findElement(accessibility))).perform();
+       // t.tap(PointOption.point(538, 416)).perform();
+        t.tap(TapOptions.tapOptions().withElement(ElementOption.element(driver.findElement(accessibility)))).perform();
+    }
+}
+//TAP, PRESS, LONGPRESS, WAITACTION, RELEASE, PERFORM, MOVETO
